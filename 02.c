d613d3ae1563fdd1bdf1d27fb5027ca57e44ff29
iwang2 <iwang2@stuy.edu>
2017-09-17 22:09:39 -0400

    completed euler problems 1 and 2

--- 02.c
+++ 02.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//for problem 1
-int check(int num, int total){
-  if(num%3 == 0){
-    return total + num;
-  }else if(num%5 == 0){
-    return total + num;
-  }else{
+int p1(int num, int total){
+  if(num >= 1000){
     return total;
+  }
+  else if(num%3 == 0 || num%5 == 0){
+    return p1(num+1, total + num);
+ }else{
+    return p1(num+1, total);
+  }
+}
+
+int p2(int num1, int num2, int total){
+  if(num2 > 4000000){
+    return total;
+  }else if(num2%2 == 0){
+    return p2(num2, num1 + num2, total + num2);
+  }else{
+    return p2(num2, num1 + num2, total);
   }
 }
 
 int main(){
-  int total;
-  for(int i = 3; i < 1000; i++){
-    total = check(i, total);
-  }
-  printf("%d\n", total);
+  //for problem 1
+  printf("The sum of all multiples of 3 and 5 less than 1000 is %d.\n", p1(0, 0));
+  printf("The sum of the even-valued terms under 4 million in the Fibonacci sequence is %d.\n", p2(1,2,0));
 }
